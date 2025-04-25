fa91ffac805d10381388bc9c94ba855ed7b363df
l-marcel <lmgh1312@gmail.com>
2025-04-25 15:26:01 -0300

    [test] Adicionando mais testes dos repositórios dos planos e usuários

--- backend/src/test/java/app/netbooks/backend/BaseTest.java
+++ backend/src/test/java/app/netbooks/backend/BaseTest.java
@@ -1,38 +1,31 @@
 package app.netbooks.backend;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestClassOrder;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import app.netbooks.backend.connections.Database;
 
 @ExtendWith(SpringExtension.class)
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@SpringBootTest
 @ActiveProfiles("test")
 public abstract class BaseTest  {
     public static void clear(Database database, String table) {
         try {
             Connection connection = database.getConnection();
-            PreparedStatement statement = connection.prepareStatement(
-                "DELETE * FROM ?;"
-            );
-            statement.setString(1, table);
-            statement.executeUpdate();
+            Statement statement = connection.createStatement();
+            statement.execute("DELETE FROM " + table + ";");
             statement.close();
             connection.close();
         } catch (SQLException e) {
