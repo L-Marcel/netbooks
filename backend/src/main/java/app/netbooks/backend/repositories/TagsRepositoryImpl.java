fbabc7f6efa1381503bc474cd548bd62eacfc94e
Marcela Silva <marcela013sb@hotmail.com>
2025-05-01 20:34:54 -0300

    [feat] Implementação do repositório

---
+++ backend/src/main/java/app/netbooks/backend/repositories/TagsRepositoryImpl.java
@@ -0,0 +1,67 @@
+package app.netbooks.backend.repositories;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import app.netbooks.backend.connections.Database;
+import app.netbooks.backend.models.Tag;
+
+public class TagsRepositoryImpl extends BaseRepository implements TagsRepository{
+
+    public TagsRepositoryImpl(Database database) {
+        super(database);
+    }
+
+    @Override
+    public void initialize() {
+        try {
+            Connection connection = this.database.getConnection();
+            Statement statement = connection.createStatement();
+
+            statement.execute(
+                "CREATE TABLE IF NOT EXISTS Tags (\n" + 
+                "    name VARCHAR(20) PRIMARY KEY\n" + 
+                ");"
+            );
+
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Tag> findAll() {
+        List<Tag> tags = new ArrayList<Tag>();
+
+        try {
+            Connection connection = this.database.getConnection();
+            Statement statement = connection.createStatement();
+
+            ResultSet result = statement.executeQuery(
+                "SELECT name FROM Tags;"
+            );
+
+            while (result.next()) {
+                String name = result.getString("name");
+
+                Tag tag = new Tag(name);
+                tags.add(tag);
+            }
+
+            result.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return tags;
+    }
+    
+}
