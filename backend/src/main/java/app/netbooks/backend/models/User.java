f26c7ac085767dccefa470994add6dc338acb64b
l-marcel <lmgh1312@gmail.com>
2025-05-05 09:29:10 -0300

    [feat] Definindo acesso padrão dos usuários

--- backend/src/main/java/app/netbooks/backend/models/User.java
+++ backend/src/main/java/app/netbooks/backend/models/User.java
@@ -25,6 +25,7 @@ public class User implements UserDetails {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.access = Access.DEFAULT;
     };
 
     @Override
