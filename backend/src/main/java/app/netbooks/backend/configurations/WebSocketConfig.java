254346c90ca17534baf32346c8414295f4508c1e
Marcela <marcela013sb@hotmail.com>
2025-05-31 22:13:52 -0300

    [feat] Configurando websocket

---
+++ backend/src/main/java/app/netbooks/backend/configurations/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package app.netbooks.backend.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocket
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+    @Override
+    public void registerStompEndpoints(
+        @SuppressWarnings("null") StompEndpointRegistry registry
+    ) {
+        registry
+            .addEndpoint("/websocket")
+            .setAllowedOrigins("*");
+    };
+
+    @Override
+    public void configureMessageBroker(
+        @SuppressWarnings("null") MessageBrokerRegistry registry
+    ){
+        registry.enableSimpleBroker("/channel/events/match");
+        registry.setApplicationDestinationPrefixes("channel/triggers/match");
+    };
+}
