d0075e660bac91ad802878ad46940e0821b9c85f
l-marcel <lmgh1312@gmail.com>
2025-07-12 22:17:43 -0300

    [fix] Removendo mais avisos

--- frontend/src/components/Grid/ParticipantsMasonryGrid.tsx
+++ frontend/src/components/Grid/ParticipantsMasonryGrid.tsx
@@ -1,5 +1,4 @@
 import { Participant } from "@stores/useRoom";
-import React from "react";
 
 interface Props {
   participants: Participant[];
@@ -8,9 +7,9 @@ interface Props {
 export default function ParticipantsMasonryGrid({ participants }: Props) {
   return (
     <div className="columns-1 sm:columns-2 md:columns-3 gap-4 space-y-4">
-      {participants.map((participant, index) => (
+      {participants.map((participant) => (
         <div
-          key={index}
+          key={participant.name}
           className="break-inside-avoid bg-gray-500 rounded-2xl p-4 shadow-md text-center font-medium text-white"
         >
           {participant.name}
